efc4bf2d4792d736a1184fce460fd9cf787ff99c
Ademola <adeoyesodeeqademola@gmail.com>
2025-02-15 16:37:47 +0100

    implemented the navbar and route method, with calces

--- src/Components/DisplayWindow.jsx
+++ src/Components/DisplayWindow.jsx
@@ -3,10 +3,8 @@ import React from "react";
 const DisplayWindow = ({ expression, result }) => {
   return (
     <div className="displayWindow">
-    <div style={{overflowX: 'auto', width: '100%'}}>
       <p className="expression mb-0">{expression ? expression : 0}</p>
       <p className="result mb-0">{result}</p>
-    </div>
     </div>
   );
 };
